93cbed812328f8ac88e6222b8a4837e4ef52830b
MS1Mehmet <73439316+MS1Mehmet@users.noreply.github.com>
2021-04-07 16:21:25 +0200

    Wolf_Enemy()
    Animation erstellt()
    Script basic enemy controlleur angefangen

---
+++ RAYZE/Assets/Scripts/Enemie/Enemy/Patroling/BasicEnemyController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemyController : MonoBehaviour
+{
+  
+
+    // Code arbeitet mit einer state machine!!
+    private enum state
+     {
+        idle,
+        walking,
+        knockback,
+        dead
+     }
+
+    private state currentState;
+
+
+    private void Update()
+    {
+        switch (currentState) 
+        {
+            case state.walking:
+                updateWalkingState();
+            break;
+            case state.knockback:
+                updateKnockbackState();
+            break;
+            case state.dead:
+                updateDeadState();
+            break;
+        }
+    }
+
+
+    //------Walking/running-- State------------------------------------------
+
+    private void enterWalkingState() 
+    { 
+    
+    }
+
+    private void updateWalkingState() 
+    {
+    
+    }
+
+    private void exitWalkingState() 
+    {
+    
+    }
+
+    //---Knockback/hurt---State-----------------------------------------------
+    private void enterKnockbackState() 
+    { 
+    
+    }
+
+    private void updateKnockbackState() 
+    {
+    
+    }
+    
+    private void exitKnockbackstate() 
+    {
+    
+    }
+
+    private void enterDeadState() 
+    {
+    
+    }
+
+    private void updateDeadState()
+    {
+
+    }
+
+  
+}
