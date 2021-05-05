423635f51530f819439d65c0ec62f6a1df30a955
giuliano1991 <giuliano_metallo@hotmail.de>
2021-05-05 17:42:10 +0200

    Der ganze geile Shit
    Alles aus meiner alten Branch in die neue JO

---
+++ RAYZE/Assets/Scripts/PlayerNew/PlayerStates/SubsStates/PlayerDamageState.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamageState : PlayerState
+{
+    private int xInput;
+    private int yInput;
+    private bool isGrounded;
+
+    public PlayerDamageState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName) : base(player, stateMachine, playerData, animBoolName)
+    {
+    }
+
+    public override void DoChecks()
+    {
+        base.DoChecks();
+        isGrounded = player.CheckIfGrounded();
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        if (!player.isDeath && player.isHit)
+        {
+            // Hier Enemy Damage übergeben
+            playerData.playerCurrentHealth -= 10;
+            player.SetVelocity(playerData.hitVelocity, playerData.hitAngle, player.FacingDirection * -1);
+        }
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        if (playerData.playerCurrentHealth <= 0)
+        {
+            stateMachine.ChangeState(player.DeathState);
+        }
+        else if (!player.isHit)
+        {
+            if (isGrounded && player.CurrentVelocity.y < 0.01f)
+            {
+                if (xInput != 0)
+                {
+                    stateMachine.ChangeState(player.MoveState);
+                }
+                else
+                {
+                    stateMachine.ChangeState(player.IdleState);
+                }
+            }
+            else if (!isGrounded)
+            {
+                stateMachine.ChangeState(player.InAirState);
+            }
+        }
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+    }
+}
---
+++ RAYZE/Assets/Scripts/PlayerNew/PlayerStates/SubsStates/PlayerDeathState.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeathState : PlayerState
+{
+    public PlayerDeathState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName) : base(player, stateMachine, playerData, animBoolName)
+    {
+    }
+
+    public override void DoChecks()
+    {
+        base.DoChecks();
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        player.StopHit();
+        player.SetDeath(true);
+        player.SetVelocityZero();
+        
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+        if (Time.time >= startTime + playerData.deathTime)
+        {
+            playerData.playerCurrentHealth = playerData.playerMaxHealth;
+            player.SetDeath(false);
+            stateMachine.ChangeState(player.IdleState);
+        }
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+    }
+}
