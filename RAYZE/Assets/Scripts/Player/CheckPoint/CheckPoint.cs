5adcaa526611e17d72e7c6de8c1b4198b9930fc6
PyesonOo <44833929+PyesonOo@users.noreply.github.com>
2021-05-06 22:06:05 +0200

    Checkpoint

---
+++ RAYZE/Assets/Scripts/Player/CheckPoint/CheckPoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    public LevelManager levelmanager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelmanager = FindObjectOfType<LevelManager>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("Checkpoint");
+            // neuen checkpoint abspeichern
+            levelmanager.currentCheckpoint = gameObject;
+        }
+        
+    }
+}
