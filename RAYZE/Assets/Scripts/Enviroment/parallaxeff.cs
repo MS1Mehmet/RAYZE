694a9e1fad083302ec7c607cf7cdbc893f9668df
MS1Mehmet <73439316+MS1Mehmet@users.noreply.github.com>
2021-05-12 16:57:48 +0200

    ParallaxEffect()

---
+++ RAYZE/Assets/Scripts/Enviroment/parallaxeff.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parallaxeff : MonoBehaviour
+{
+
+    private float lenght, startPos;
+    public GameObject cam;
+    public float parallax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position.x;
+        lenght = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float temp = (cam.transform.position.x * (1- parallax));
+        float dist = (cam.transform.position.x * ( parallax));
+        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+
+        if(temp > startPos + lenght)
+        {
+            startPos += lenght;
+        }
+        else if (temp < startPos - lenght) 
+        {
+            startPos -= lenght;
+        }
+    }
+}
