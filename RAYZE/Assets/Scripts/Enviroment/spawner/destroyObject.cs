0ac1caba912d6bc747ef06f5f123c04115063765
MS1Mehmet <73439316+MS1Mehmet@users.noreply.github.com>
2021-06-05 13:36:16 +0200

    Enemies and Enviremount fixes

---
+++ RAYZE/Assets/Scripts/Enviroment/spawner/destroyObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyObject : MonoBehaviour
+{
+    [SerializeField]
+    float destroyTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, destroyTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
