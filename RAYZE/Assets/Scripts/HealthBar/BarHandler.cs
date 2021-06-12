e0307a4c70f991d45f2a3f801b479605da149830
Djolevic541 <74978975+Djole541@users.noreply.github.com>
2021-06-12 16:53:49 +0200

    HealthBar
    UI für Healthbar

---
+++ RAYZE/Assets/Scripts/HealthBar/BarHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class BarHandler : MonoBehaviour
+{
+   [SerializeField] private HealthBar healthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float health = 1f;
+        bool changeColor = true;
+       
+        FunctionPeriodic.Create(() =>
+        {
+            if(health > .01f)
+            {
+                health -= .01f;
+                healthBar.SetSize(health);
+
+                if(health < .3f)
+                {
+                    if (changeColor)
+                    {
+                        healthBar.SetColor(Color.red);
+                        changeColor = false;
+                    }
+                    else
+                    {
+                        healthBar.SetColor(Color.white);
+                        changeColor = true;
+                    }
+
+                }
+            }
+        },0.05f);
+        
+    }
+    
+  
+}
