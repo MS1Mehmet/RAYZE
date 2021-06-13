140137a27f7e08176aea69ab80a063b3883746d1
MS1Mehmet <73439316+MS1Mehmet@users.noreply.github.com>
2021-06-13 15:15:51 +0200

    LevelDesign()

---
+++ RAYZE/Assets/Scripts/GameFunctions/Scene Changer2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneChanger2 : MonoBehaviour
+{
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void ResetScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneChanger2.LoadScene
+    }
+}
