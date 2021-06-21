1843413209713b80966023e18bcbfd158ac726eb
PyesonOo <44833929+PyesonOo@users.noreply.github.com>
2021-06-21 15:35:08 +0200

    OneWayPlattform

---
+++ RAYZE/Assets/Scripts/OneWay/VerPlattform.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerPlattform : MonoBehaviour
+{
+    [SerializeField]
+    private Player player;
+
+    private PlatformEffector2D effector;
+    public float waitTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        effector = GetComponent<PlatformEffector2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.InputHandler.NormInputY == -1)
+        {
+            waitTime = 0.5f;
+
+        }
+        if (player.InputHandler.NormInputY == -1)
+        {
+            if(waitTime <= 0)
+            {
+                effector.rotationalOffset = 150f;
+                waitTime = 0.5f;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            effector.rotationalOffset = 0;
+        }
+    }
+}
