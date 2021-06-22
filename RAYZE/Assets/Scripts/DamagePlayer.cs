fb8305b7bba86af3c257181744f6053017553018
MS1Mehmet <73439316+MS1Mehmet@users.noreply.github.com>
2021-06-22 18:13:56 +0200

    CameraFix() EnemyControlleur()
    Kamera konnte nicht vollständig gefxied werden somit geht der scene change nur in eine Richtung.

---
+++ RAYZE/Assets/Scripts/DamagePlayer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePlayer : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    [SerializeField]
+    public Player playerScr;
+    public int hitDamage;
+
+    public int health;
+    BoxCollider2D box2d;
+    [SerializeField]
+    GameObject deathExplosion;
+
+    SpriteRenderer sprite;
+    GameObject explodeEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+       
+        box2d = GetComponent<BoxCollider2D>();
+        rb2d = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Damage(int amount)
+    {
+        health -= amount;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject, 1f);
+            explodeEffect = Instantiate(deathExplosion);
+            explodeEffect.name = deathExplosion.name;
+            explodeEffect.transform.position = sprite.bounds.center;
+            Destroy(explodeEffect, 1.2f);
+        }
+        /*else
+        {
+            EnemyAnimation.Play("hurt");
+        }*/
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerScr.DamageState.PlayerTakeDamage(hitDamage);
+            playerScr.SetTakenHit(true);
+            Debug.Log(hitDamage);
+        }
+    }
+}
